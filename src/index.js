b502368867035730ab9fbad5654ab1cb639a0a65
Lali Gulbani <lal.gulbani@gmail.com>
2020-08-05 21:05:28 +0300

    HW_1 Setup Webpack.

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+import './index.css';
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <h1>Hello World!</h1>,
+  rootElement
+);
